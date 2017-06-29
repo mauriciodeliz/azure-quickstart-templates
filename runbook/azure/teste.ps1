fba9cb880ede4725f7850c3145b18461ba6aa564
Mauricio de Liz <maurifut@hotmail.com>
2017-06-29 11:57:03 -0300

    Created teste.ps1 in AzureAutomation.

---
+++ runbook/azure/teste.ps1
@@ -0,0 +1,4 @@
+$myCredential = Get-AutomationPSCredential -Name 'MyCredential'
+$userName = $myCredential.UserName
+$securePassword = $myCredential.Password
+$password = $myCredential.GetNetworkCredential().Password
